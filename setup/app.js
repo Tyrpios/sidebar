eb2560d9fa361de060fa1e110b6d1055420d9348
Tyrpios <enminata@gmail.com>
2020-09-08 20:52:15 -0500

    sidebar

---
+++ setup/app.js
@@ -0,0 +1,12 @@
+const toggleroo = document.querySelector(".sidebar-toggle");
+const closeroo = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleroo.addEventListener("click", ()=>{
+    console.log(sidebar.classList);
+    if (sidebar.classList.contains("show-sidebar")) {
+        sidebar.classList.remove("show-sidebar");
+    } else {
+        sidebar.classList.add("show-sidebar");
+    }
+});
